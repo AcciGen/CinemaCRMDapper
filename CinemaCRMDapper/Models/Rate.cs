d240da55e12da513b0e7a0cd73c70e47cc399011
Nuriddin Asrorov <denrov13@gmail.com>
2024-02-15 19:43:31 +0500

    Create models

---
+++ CinemaCRMDapper/Models/Rate.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaCRMDapper.Models
+{
+    public class Rate
+    {
+        public int id { get; set; }
+        public short avgRate { get; set; }
+        public int movieId { get; set; }
+    }
+}
---
+++ CinemaCRMDapper/Models/Ticket.cs
@@ -0,0 +1,8 @@
+﻿namespace CinemaCRMDapper.Models
+{
+    public class Ticket
+    {
+        public int id { get; set; }
+        public int price { get; set; }
+    }
+}
