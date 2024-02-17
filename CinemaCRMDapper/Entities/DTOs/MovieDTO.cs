afb081a4a7c15af1b6e5a398de3b53696faeee27
Nuriddin Asrorov <denrov13@gmail.com>
2024-02-17 18:00:50 +0500

    Small fixes

--- CinemaCRMDapper/Entities/DTOs/MovieDTO.cs
+++ CinemaCRMDapper/Entities/DTOs/MovieDTO.cs
@@ -2,7 +2,7 @@
 {
     public class MovieDTO
     {
-        public string title { get; set; }
+        public string? title { get; set; }
         public int year { get; set; }
         public long budget { get; set; }
         public float avgRate { get; set; }
--- CinemaCRMDapper/Models/Movie.cs
+++ CinemaCRMDapper/Models/Movie.cs
@@ -3,7 +3,7 @@
     public class Movie
     {
         public int Id { get; set; }
-        public string title { get; set; }
+        public string? title { get; set; }
         public int year { get; set; }
         public long budget { get; set; }
         public float avgRate { get; set; }
